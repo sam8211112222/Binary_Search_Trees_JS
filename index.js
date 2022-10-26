bebaebee2bb1dcf153f26fee9c02b5ec17b3be70
Sam Chen <sam8211112222@gmail.com>
2022-10-26 11:39:17 +0800

    Recursion test

---
+++ index.js
@@ -0,0 +1,10 @@
+// Factorial
+// 4! = 4 * 3 * 2 * 1 = 24
+// 3! = 3 * 2 * 1 = 6
+function factorial(num) {
+    if (num === 1) {
+        return num
+    } else {
+        return num * factorial(num - 1);
+    }
+}
